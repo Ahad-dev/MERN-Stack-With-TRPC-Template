b0d7535be14de9d735d6d41a749de991c7f2146d
Ahad <muhammadahad211126@gmail.com>
2025-07-14 19:14:51 +0500

    Make Effiecint Add Logout Functionality

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -2,13 +2,25 @@ import React from 'react'
 import UserSidebar from '@/modules/dashboard/ui/components/UserSidebar'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import DashboardNavbar from '@/modules/dashboard/ui/components/dashboard-navbar'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigate } from 'react-router'
+import { authClient } from '@/lib/auth'
 
 
 const Layout = () => {
+  const {data:session,isPending} = authClient.useSession()
+  const navigate = useNavigate()
+
+  if (isPending) {
+    return <div>Loading...</div>
+  }
+  console.log("Session data:", session);
+
+  if (!session) {
+    navigate('/sign-up')
+  }
+
   return (
     <div>
-      
       <SidebarProvider>
         <UserSidebar/>  
         <main className='flex flex-col h-screen w-screen'>
--- frontend/src/pages/sign-up/page.tsx
+++ frontend/src/pages/sign-up/page.tsx
@@ -1,21 +1,34 @@
-import { authClient } from "@/lib/auth"
+import { authClient } from "@/lib/auth";
+import { useNavigate } from "react-router";
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const { data: session, isPending } = authClient.useSession();
 
 
-    const signIn = async () => {
-        const data = await authClient.signIn.social({
-            provider: "google",
-            callbackURL: 'http://localhost:5173/',
+
+  const signIn = async () => {
+    const data = await authClient.signIn.social(
+      {
+        provider: "google",
+        callbackURL: "http://localhost:5173/",
+      },
+      {
+        onError: (error) => {
+          console.error("Sign in error:", error);
         },
-        {
-            onError: (error) => {
-                console.error("Sign in error:", error);
-            }
-        }
-    )
-        console.log("Sign in data:", data);
-    }
+      }
+    );
+    console.log("Sign in data:", data);
+  };
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (session) {
+    navigate("/");
+  }
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold mb-4">Sign Up</h1>
@@ -26,8 +39,7 @@ const Signup = () => {
         Sign Up with Google
       </button>
     </div>
+  );
+};
 
-  )
-}
-
-export default Signup
+export default Signup;
